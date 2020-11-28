034128635672428e7e223db428782abf8a46c192
Haust891 <32645072+Haust891@users.noreply.github.com>
2020-11-28 09:29:30 +0100

    Battlemap_project

--- Token.gd
+++ Token.gd
@@ -2,9 +2,18 @@ extends Node2D
 
 var selected = false
 
+func _ready():
+	$Button.disabled = true
+	$Button.visible = false
+
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("left_click"):
 		selected = true
+		$Button.disabled = true
+		$Button.visible = false
+	if Input.is_action_just_pressed("right_click"):
+		$Button.disabled = false
+		$Button.visible = true
 		
 func _process(delta):
 	if selected:
@@ -14,3 +23,7 @@ func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and not event.pressed:
 			selected = false
+
+
+func _on_Button_pressed():
+	queue_free()
--- root.gd
+++ root.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
+var token = preload("res://Token.tscn")
+
 func _ready():
-	pass # Replace with function body.
+	pass 
 
 func _process(delta):
 	pass
@@ -16,6 +17,20 @@ func _on_FileDialog_file_selected(path):
 	var image_texture = ImageTexture.new()
 	image.load(path)
 	image_texture.create_from_image(image)
-	$TextureRect/Sprite.texture = image_texture 
+	$Sprite.texture = image_texture 
 	
 	
+
+
+func _on_CreateToken_pressed():
+	var instancedtoken = token.instance()
+	add_child(instancedtoken)
+	$FileDialog2.popup()
+
+	# Move the imgage selection to a button in the Token instead! easy fix instead of arraying and grouping?
+func _on_FileDialog2_file_selected(path):
+	var tokenimage = Image.new()
+	var tokenimage_texture = ImageTexture.new()
+	tokenimage.load(path)
+	tokenimage_texture.create_from_image(tokenimage)
+	$Token/Sprite.texture = tokenimage_texture
